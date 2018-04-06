e848498da08d638bc7cd4445ebcfebaa25001417
Jack Steele <jsteele883@gmail.com>
2018-04-05 17:34:28 -0700

    Set up with webpack

---
+++ public/js/main.js
@@ -0,0 +1,68 @@
+// API fetching and splitting
+
+// Get data with JQuery (should be cleaned up to use ES6)
+let coins = $.when(
+  $.getJSON('https://api.coinmarketcap.com/v1/ticker/')
+).done( function(coins) {
+    console.log(coins);
+
+const coin = coins.find(coin => {
+
+  const markup = `
+    ${coins.map(coin => `
+                        <button class="Accordion" role="tab" aria-selected="false">${coin.name}</button>
+                        <div class="Rtable-cell Rtable-cell--head">
+                          <h2>${coin.name}</h2>
+                        </div>
+                        <div class="Rtable-cell">
+                          <p>${coin.price_usd}USD</p>
+                        </div>
+                        <div class="Rtable-cell">
+                          <p>${coin.price_btc}BTC</p>
+                        </div>
+                        <div class="Rtable-cell Rtable-cell--foot">
+                          <p>Here's a description</p>
+                        </div>`).join('')}
+  `;
+  const tableContainer = document.getElementById('tableContainer');
+  tableContainer.innerHTML = markup;
+});
+
+// JQuery for toggle functionality
+(function ($) {
+  "use strict";
+  $.fn.responsiveTable = function() {
+
+    var toggleColumns = function($table) {
+      var selectedControls = [];
+      $table.find(".Accordion, .Tab").each( function() {
+        selectedControls.push( $(this).attr("aria-selected") );
+      });
+      var cellCount = 0, colCount = 0;
+      var setNum = $table.find(".Rtable-cell").length / Math.max( $table.find(".Tab").length, $table.find(".Accordion").length );
+      $table.find(".Rtable-cell").each( function() {
+        $(this).addClass("hiddenSmall");
+        if( selectedControls[colCount] === "true" ) $(this).removeClass("hiddenSmall");
+        cellCount++;
+        if( cellCount % setNum === 0 ) colCount++;
+      });
+    };
+    $(this).each(function(){ toggleColumns($(this)); });
+
+    $(this).find(".Tab").click( function() {
+      $(this).attr("aria-selected","true").siblings().attr("aria-selected","false");
+      toggleColumns( $(this).parents(".Rtable") );
+    });
+
+    $(this).find(".Accordion").click( function() {
+      $(this).attr("aria-selected", $(this).attr("aria-selected") !== "true" );
+      toggleColumns( $(this).parents(".Rtable") );
+    });
+
+  };
+}(jQuery));
+
+
+$(".js-RtableTabs, .js-RtableAccordions").responsiveTable();
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+  entry: './public/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public/js/'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  }
+};
