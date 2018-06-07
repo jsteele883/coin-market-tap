26b4f041c57c3fb9fcb3445d4d409878b5e635ac
Jack Steele <jsteele883@gmail.com>
2018-06-07 18:06:04 +0100

    created render accordions as it's own function

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import '../sass/style.scss';
-import getApiData from './combine-data';
+import { apiUrl } from './config';
 import { coinDescriptions } from './config';
 import _ from 'lodash';
 import { tableToggle } from './responsive-table';
@@ -22,81 +22,80 @@ function replaceIcons(coinSymbol) {
   });
 }
 
-getApiData
-  .then(coins => coins.json())
-  .then(coins => {
-    // combine coin descriptions with api data
-    const combinedData = _.map(coins, function(obj) {
-      return _.assign(obj, _.find(coinDescriptions, {name: obj.name}));
-    });
-
-    console.log(combinedData);
-
-    // Get coins with no descriptions
-    const undefinedDescriptions = combinedData.map(coin => {
-      if (coin.description === undefined) {
-        return coin.name
+// Filter out the undefined ones
+function filter_array(test_array) {
+  var index = -1,
+      arr_length = test_array ? test_array.length : 0,
+      resIndex = -1,
+      result = [];
+  while (++index < arr_length) {
+      var value = test_array[index];
+      if (value) {
+          result[++resIndex] = value;
       }
-    });
+  }
+  return result;
+}
 
-    // Filter out the undefined ones
-    function filter_array(test_array) {
-      var index = -1,
-          arr_length = test_array ? test_array.length : 0,
-          resIndex = -1,
-          result = [];
-      while (++index < arr_length) {
-          var value = test_array[index];
-          if (value) {
-              result[++resIndex] = value;
-          }
-      }
-      return result;
-    }
+function renderAccordions(markup, order) {
+  const getApiData = fetch(apiUrl);
+  getApiData
+    .then(coins => coins.json())
+    .then(coins => {
+      // combine coin descriptions with api data
+      const combinedData = _.map(coins, function(obj) {
+        return _.assign(obj, _.find(coinDescriptions, {name: obj.name}));
+      });
 
-    // Need to email results.
-    console.log(filter_array(undefinedDescriptions));
-
-    // apply markup
-    const tableContainer = document.getElementById('tableContainer');
-    tableContainer.innerHTML = `${combinedData.map(accordionMarkup).join('')}`;
-
-    // apply toggle Functionality to each coin
-    tableToggle();
-
-    replaceIcons();
-
-    // tab toggling function
-    (function tabClick() {
-      const tabContainer = document.getElementById("tabContainer");
-      const tab = tabContainer.getElementsByClassName("Tab");
-      // toggle between tabs
-      for(var i = 0; i< tab.length; i++) {
-        tab[i].addEventListener("click", function() {
-          let current = document.getElementsByClassName("active");
-          current[0].className = current[0].className.replace(" active", "");
-          this.className += " active";
-          let selectedTab = (this.id);
-          // sort order and determine mark up according to tab selection
-          if (selectedTab == 'rank') {
-            combinedData.sort((a, b) => parseFloat(a.rank) - parseFloat(b.rank));
-            tableContainer.innerHTML = `${combinedData.map(accordionMarkup).join('')}`;
-          } else if (selectedTab == 'one') {
-            combinedData.sort((a, b) => parseFloat(b.percent_change_1h) - parseFloat(a.percent_change_1h));
-            tableContainer.innerHTML = `${combinedData.map(accordionMarkupOne).join('')}`;
-          } else if (selectedTab == 'two') {
-            combinedData.sort((a, b) => parseFloat(b.percent_change_24h) - parseFloat(a.percent_change_24h));
-            tableContainer.innerHTML = `${combinedData.map(accordionMarkup).join('')}`;
-          } else if (selectedTab == 'three') {
-            combinedData.sort((a, b) => parseFloat(b.percent_change_7d) - parseFloat(a.percent_change_7d));
-            tableContainer.innerHTML = `${combinedData.map(accordionMarkupWeek).join('')}`;
-          }
-          tableToggle();
-          replaceIcons();
-        });
-      }
-    })();
+      // sort coins
+      combinedData.sort((a, b) => parseFloat(a.rank) - parseFloat(b.rank));
+      // Get coins with no descriptions
+      const undefinedDescriptions = combinedData.map(coin => {
+        if (coin.description === undefined) {
+          return coin.name
+        }
+      });
+      // Need to email results.
+      console.log(filter_array(undefinedDescriptions));
+      // apply markup
+      const tableContainer = document.getElementById('tableContainer');
+      tableContainer.innerHTML = `${combinedData.map(markup).join('')}`;
+      // apply toggle Functionality to each coin
+      tableToggle();
+      replaceIcons();
+    })
+    .catch((err) => {
+      console.log(err);
   })
-  .catch((err) => {
-    console.log(err);
-  })
+}
+
+renderAccordions(accordionMarkup);
+
+// tab toggling function
+(function tabClick() {
+  const tabContainer = document.getElementById("tabContainer");
+  const tab = tabContainer.getElementsByClassName("Tab");
+  // toggle between tabs
+  for(var i = 0; i< tab.length; i++) {
+    tab[i].addEventListener("click", function() {
+      let current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+      let selectedTab = (this.id);
+      // sort order and determine mark up according to tab selection
+      if (selectedTab == 'rank') {
+        // combinedData.sort((a, b) => parseFloat(a.rank) - parseFloat(b.rank));
+        renderAccordions(accordionMarkup)
+      } else if (selectedTab == 'one') {
+        // combinedData.sort((a, b) => parseFloat(b.percent_change_1h) - parseFloat(a.percent_change_1h));
+        renderAccordions(accordionMarkupOne);
+      } else if (selectedTab == 'two') {
+        // combinedData.sort((a, b) => parseFloat(b.percent_change_24h) - parseFloat(a.percent_change_24h));
+        renderAccordions(accordionMarkup);
+      } else if (selectedTab == 'three') {
+        // combinedData.sort((a, b) => parseFloat(b.percent_change_7d) - parseFloat(a.percent_change_7d));
+        renderAccordions(accordionMarkupWeek);
+      }
+    });
+  }
+})();
